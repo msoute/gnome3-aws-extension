b855b8b233f45ce093091278098449cb8ead6581
Marcel Soute <m.soute@gmail.com>
2017-09-15 11:45:47 +0200

    Add InstanceID to connect menu. Add Initial Filter menu

--- src/awsUtil.js
+++ src/awsUtil.js
@@ -1,20 +1,27 @@
 const Me = imports.misc.extensionUtils.getCurrentExtension();
 const GLib = imports.gi.GLib;
+const Gio = imports.gi.Gio;
 
-function listInstances(settings) {
+let out_reader;
+let asyncCall;
+
+let resultJson = "";
+
+function listInstances(settings, callback) {
+    asyncCall = callback;
     if (settings['aws_filter_tag_value'].length === 0) {
         return undefined;
     }
-    let filterValue  = settings['aws_filter_tag_value'];
+    let filterValue = settings['aws_filter_tag_value'];
 
-    let [res, out, err, status] = GLib.spawn_command_line_sync("aws --profile "+settings['aws_cli_profile']+" ec2 describe-instances --output json --filters Name=tag:Name,Values="+filterValue+" --query 'Reservations[*].Instances[*].{PublicIp:PublicIpAddress, PrivateIp:PrivateIpAddress, Tag:Tags, InstanceId:InstanceId,State:State.Name}'");
+    let array = ['/usr/bin/aws', '--profile', settings['aws_cli_profile'], 'ec2', 'describe-instances', '--output', 'json', '--filters', "Name=tag:Name,Values=" + filterValue, '--query', 'Reservations[*].Instances[*].{PublicIp:PublicIpAddress, PrivateIp:PrivateIpAddress, Tag:Tags, InstanceId:InstanceId,State:State.Name}'];
 
-    if (status !== 0) {
-        throw err;
-    }
-    let jsonResponse = JSON.parse(out.toString());
-    jsonResponse.sort(function(a,b) { return findTag(a, "Name").localeCompare(findTag(b, "Name")) } );
-    return jsonResponse;
+    resultJson = "";
+    let [res_async, pid, in_fd, out_fd, err_fd] = GLib.spawn_async_with_pipes(null, array, null, 0, null);
+    out_reader = new Gio.DataInputStream({
+        base_stream: new Gio.UnixInputStream({fd: out_fd})
+    });
+    out_reader.read_upto_async("", 0, 0, null, _SocketRead, "");
 }
 
 function terminateInstance(instanceId, settingsJSON) {
@@ -25,6 +32,20 @@ function terminateInstance(instanceId, settingsJSON) {
     } else {
         global.log("Unable to terminate instanceId, no instanceId value");
         return false;
+    }
+}
+
+function _SocketRead(source_object, res) {
+    const [out, length] = out_reader.read_upto_finish(res);
+    if (length > 0) {
+        resultJson = resultJson + out;
+        out_reader.read_upto_async("", 0, 0, null, _SocketRead, "");
+    } else {
+        let jsonResponse = JSON.parse(resultJson);
+        jsonResponse.sort(function (a, b) {
+            asyncCall(findTag(a, "Name").localeCompare(findTag(b, "Name")));
+            //return findTag(a, "Name").localeCompare(findTag(b, "Name"))
+        });
     }
 }
 
